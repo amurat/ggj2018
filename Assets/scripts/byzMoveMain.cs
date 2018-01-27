c7e944ad648b408f42b3b2083fb6b19dc1e94229
Altay Murat <amurat@echelon-software.com>
2018-01-27 13:39:49 -0500

    character movement controller

---
+++ Assets/scripts/byzMoveMain.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class byzMoveMain : MonoBehaviour {
+
+	public GameObject arrow;
+	public AudioClip fireArrowAudioClip;
+	public float velocity = 10;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//movement controls for left, right, and jumping
+		if ((Input.GetKey (KeyCode.LeftArrow))){	
+			transform.position += new Vector3(-1f, 0f, 0f) * velocity *Time.deltaTime;
+		}
+		if ((Input.GetKey (KeyCode.RightArrow))){
+			transform.position += new Vector3(1f, 0f, 0f) * velocity *Time.deltaTime;
+		}
+		if ((Input.GetKey (KeyCode.DownArrow))){
+		}
+		if ((Input.GetKeyDown (KeyCode.UpArrow))){	//jump limited by a raycast downward
+			GetComponent<AudioSource>().PlayOneShot (fireArrowAudioClip);
+		}
+		//reset the level
+		if (Input.GetKeyDown (KeyCode.R)){		
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
